13302c1f9503e504ac46c94446db19c6b3e28713
xj <xj@email.com>
2022-10-04 12:14:22 +0300

    add categories id

--- update-prices.js
+++ update-prices.js
@@ -4,8 +4,19 @@ const parser = new xml2js.Parser()
 const builder = new xml2js.Builder({ cdata: true })
 const path = require('./shared/path')
 
+const input_file_name = path.input.yml //'tilda-feed'
+const output_file_name = path.input.yml //'tilda-feed'
+const output_folder = path.output.folder //'output'
+
+const input_file_data = fs.readFileSync(
+    `./${path.input.folder}/${input_file_name}`
+)
+
 const updatePrice = (price, percent) => Math.ceil(+price + price * percent)
 
+// todo:
+// extract categories from feed insted of changesList
+// make possible filters like: include/exclude cat; set price + % to each category individually
 const changesList = [
     {
         categoryID: '907149595291',
@@ -15,46 +26,127 @@ const changesList = [
     {
         categoryID: '903569256651',
         percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '866426313361',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '444943533321',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '456028571861',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '287161144321',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '658530553911',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '910272790511',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '441079117491',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '443169019671',
+        percentToAddToThePrice: 0.05,
         active: false
+    },
+    {
+        categoryID: '439103222771',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '715825165071',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '742941910481',
+        percentToAddToThePrice: 0.05,
+        active: false
+    },
+    {
+        categoryID: '320586217181',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '516822319031',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '188559425471',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '466871267201',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '300028999651',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '635698216721',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '426550774261',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '152485699881',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '720640154791',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '847107982971',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '647536114381',
+        percentToAddToThePrice: 0.05,
+        active: true
+    },
+    {
+        categoryID: '798974541701',
+        percentToAddToThePrice: 0.05,
+        active: true
     }
 ]
+
 const categories = changesList.map((category) => category.categoryID)
-console.log(categories)
-
-const beer = require('./feed_data/beer')
-const ru_cans = [
-    '647977965791',
-    '585033696611',
-    '204949597221',
-    '193868340851',
-    '554583485411',
-    '111735614071',
-    '432736792841',
-    '822403725721',
-    '974512923591'
-] // dobroflot
-const caviar = [
-    '449478458271',
-    '732519722811',
-    '521676464691',
-    '612843888421',
-    '472843531421',
-    '274404744471',
-    '639176784161',
-    '515567209421',
-    '833511309741',
-    '834978246171',
-    '160445044221'
-]
-const products_tobe_removed = [].concat(beer, ru_cans, caviar)
-
-const input_file_name = path.input.yml //'tilda-feed'
-const output_file_name = path.input.yml //'tilda-feed'
-const output_folder = path.output.folder //'output'
-
-const input_file_data = fs.readFileSync(
-    `./${path.input.folder}/${input_file_name}`
-)
+//console.log(categories)
 
 parser.parseString(input_file_data, function (err, result) {
     const offers = result.yml_catalog.shop[0].offers[0].offer
@@ -83,5 +175,4 @@ parser.parseString(input_file_data, function (err, result) {
     fs.writeFileSync(`./${output_folder}/${output_file_name}`, xml)
 
     console.log('prices updated')
-    //console.log('offers', offers.length)
 })
