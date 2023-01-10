f4160a311a71e3e3a02623f35207a72344c43d30
xj <xj@email.com>
2023-01-11 01:32:02 +0200

    upd vseua.js path

--- vseua.js
+++ vseua.js
@@ -1,27 +1,25 @@
 const fs = require('fs')
 const parse = require('csv-parse/lib/sync')
 const stringify = require('csv-stringify/lib/sync')
-const path = require('./shared/path')
 
-const input_file_name = path.vseua.input.file //'tilda-feed'
-const output_file_name = path.vseua.output.file //'vseua-feed'
-const output_folder = path.output.folder //'output'
+const path = require('./common/path')
+const inputFilePath = path.vseua.input
+const outputFilePath = path.vseua.output
 
-const csv_file_data = fs.readFileSync(`./${path.input.folder}/${input_file_name}`)
+const feedData = fs.readFileSync(inputFilePath)
 
-const products_list = parse(csv_file_data, {
-  columns: true,
-  skip_empty_lines: true
+const products_list = parse(feedData, {
+    columns: true,
+    skip_empty_lines: true
 })
 
 // Add Quantity: stock
-products_list.forEach(product => !product.Quantity ? product.Quantity = 'stock' : '')
-
-// Make output dir
-if (!fs.existsSync(`./${output_folder}`)) fs.mkdirSync(`./${output_folder}`)
+products_list.forEach((product) =>
+    !product.Quantity ? (product.Quantity = 'stock') : ''
+)
 
 // Build csv
 const products_list_stringified = stringify(products_list, { header: true })
-fs.writeFileSync(`./${output_folder}/${output_file_name}`, products_list_stringified)
+fs.writeFileSync(outputFilePath, products_list_stringified)
 
 console.log('vseua feed done')
