ce9655dd92e5c6c59b8f1115cf87f8763d01b7a2
xj <xj@email.com>
2023-02-15 04:05:43 +0200

    upd oldprice

--- handlers/aveopt/change-prices.js
+++ handlers/aveopt/change-prices.js
@@ -3,5 +3,6 @@ const markup = 1.5
 module.exports = (offers) =>
     offers.map((offer) => {
         offer.price = Math.ceil(offer.price * markup)
+        offer.oldprice = Math.ceil(offer.oldprice * markup)
         return offer
     })
