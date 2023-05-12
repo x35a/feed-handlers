7bd93db981a5c2382952c9075366390d30a5e039
xj <xj@email.com>
2023-05-12 03:10:51 +0300

    upd tldYMLLink

--- common/const.js
+++ common/const.js
@@ -1,6 +1,6 @@
 module.exports = {
     tldYMLLink:
-        'https://smartfood.org.ua/tstore/yml/96283d7854beada45245c1187fac3dd2.yml',
+        'https://smartfood.org.ua/tstore/yml/0bfa410f29e86a9b1f651f19626451ae.yml',
     tldCSVLink: 'https://smartfood.org.ua/feed-fb.csv',
     aveoptYMLLink:
         'https://aveon.net.ua/products_feed.xml?hash_tag=7b71fadcc4a12f03cf26a304da032fba&sales_notes=&product_ids=&label_ids=&exclude_fields=&html_description=0&yandex_cpa=&process_presence_sure=&languages=ru&group_ids='
