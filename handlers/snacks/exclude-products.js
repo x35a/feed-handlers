bf7ef7a80dbb967ad03eb20049e84e213e50e510
xj <xj@email.com>
2023-07-30 03:02:18 +0300

    upd wix handler

---
+++ handlers/snacks/exclude-products.js
@@ -0,0 +1,3 @@
+module.exports = {
+    excludeById: ['e40487a9-1425-d3cc-bef5-bd167a6f39dc']
+}
--- handlers/snacks/wix-fetch-tsv.js
+++ handlers/snacks/wix-fetch-tsv.js
@@ -3,20 +3,32 @@ const parse = require('csv-parse/lib/sync')
 const stringify = require('csv-stringify/lib/sync')
 const fetchFeed = require('../../common/fetch-feed')
 const feedOutputPath = 'output/wix-feed.csv'
-const { wixTSVUrl, markup } = require('./wix-feed-settings')
+const { wixTSVUrl, markup } = require('./settings')
+const { excludeById } = require('./exclude-products')
 
 ;(async () => {
     // Get tsv
     const text = await fetchFeed(wixTSVUrl)
     if (!text) return `Fetching Error ${wixTSVUrl}`
-    //const text = fs.readFileSync('./handlers/snacks/feed.tsv') // get feed from static file
+    // const text = fs.readFileSync('./handlers/snacks/feed.tsv') // get feed from static file
 
     // Parse .tsv data
-    const productsList = parse(text, {
+    let productsList = parse(text, {
         delimiter: '\t',
         columns: true,
         quote: false, // fix error CsvError: Invalid Opening Quote: a quote is found inside a field at line .. // its better to use relax_quotes option but it didn't work mb because csv lib version is outdated https://csv.js.org/parse/options/relax_quotes/
         skip_empty_lines: true
+    })
+
+    // Filter products
+    productsList = productsList.filter((product) => {
+        // Exclude by ID
+        const allowedId = !excludeById.includes(product.id)
+
+        // Exclude out of stock
+        const inStock = product.availability !== 'out of stock'
+
+        return allowedId && inStock
     })
 
     productsList.forEach((product, index) => {
