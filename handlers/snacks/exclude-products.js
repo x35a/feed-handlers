716217762d1898ec7d31dc53175a03de46002658
xj <xj@email.com>
2023-08-10 15:04:08 +0300

    upd markup

--- handlers/snacks/exclude-products.js
+++ handlers/snacks/exclude-products.js
@@ -1,3 +1,5 @@
 module.exports = {
-    excludeById: ['e40487a9-1425-d3cc-bef5-bd167a6f39dc']
+    excludeById: [
+        'e40487a9-1425-d3cc-bef5-bd167a6f39dc' // Креветка варено-морожена 90-120 ( лише по місту Дніпро)
+    ]
 }
