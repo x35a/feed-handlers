959dcf05e391de936c519c3ea88ba5c034daaaad
xj <xj@email.com>
2023-10-24 20:18:05 +0300

    snacks upd markup

--- handlers/snacks/settings.js
+++ handlers/snacks/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
     wixTSVUrl:
         'https://manage.wix.com/catalog-feed/v1/feed.tsv?marketplace=google&version=1&token=at7rMOXsoIy9Sxvf9FdA2BbAq12yaa%2BqMr8FW6N01ZFoYommMHbQCGS4DpHFpi4s&productsOnly=false', // feed url
-    markup: 1.25
+    markup: 1.5
 }
